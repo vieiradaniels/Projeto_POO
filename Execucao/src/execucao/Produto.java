c671ae8058c7c3989fb7edf6962b3a154b2c5652
vieiradaniels <135739053+vieiradaniels@users.noreply.github.com>
2024-04-01 19:15:44 -0300

    aula27-03

---
+++ Execucao/src/execucao/Produto.java
@@ -0,0 +1,38 @@
+package execucao;
+
+public class Produto {
+
+    private int id;
+    private String descricao;
+    private double preco;
+
+    public Produto(int id, String descricao, double preco) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
