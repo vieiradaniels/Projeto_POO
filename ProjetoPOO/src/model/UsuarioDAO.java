10f6caaaae21340702e1ffd28e965baf798eacd1
vieiradaniels <135739053+vieiradaniels@users.noreply.github.com>
2024-04-02 18:50:16 -0300

    correção usuárioDAO

--- ProjetoPOO/src/model/UsuarioDAO.java
+++ ProjetoPOO/src/model/UsuarioDAO.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author admin
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class UsuarioDAO {
-    
+
+    public boolean autenticar(String email, String senha) {
+        String sql = "SELECT * FROM tbusuario" + "WHERE email = ? and senha = md5(?" + "and ativo = true";
+
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        Connection con = gerenciador.getConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            gerenciador.closeConnection(stmt, rs);
+        }
+        return false;
+    }
 }
